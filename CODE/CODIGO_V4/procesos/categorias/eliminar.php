03b6ea831e0cb442a84e5a35430c58e0f99841ec
Saul Insuasti <116695679+Saul7211@users.noreply.github.com>
2023-08-01 22:56:48 -0500

    CODIGO_V4

--- CODE/CODIGO_V4/procesos/categorias/eliminar.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../clases/Categoria.php';
-require_once '../../clases/Conexion.php';
-$id = $_POST['id'];
-$obj = new Categoria();
-echo $obj->delete($id);
-?>
--- CODE/CODIGO_V4/procesos/categorias/traer.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../clases/Categoria.php';
-require_once '../../clases/Conexion.php';
-$id = $_GET['id_categoria'];
-$obj = new Categoria();
-echo json_encode($obj->traer($id));
-?>
